be1a68dd32bbffadcf943340b98311071705e0b0
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-22 19:33:19 -0400

    exclude a few methods

--- lib/capistrano_extensions/files.rb
+++ lib/capistrano_extensions/files.rb
@@ -4,7 +4,11 @@ module CapistranoExtensions
   module Files
 
     include FileUtils
-    public *FileUtils.methods(false)
+
+    public(*(FileUtils.methods(false) - %w(copy_entry copy_file copy_stream
+                                            remove_entry remove_entry_secure remove_file
+                                            compare_file compare_stream
+                                            uptodate?)))
 
     class_eval(%w(exists? directory? executable?).map do |m|
       "def #{k}(f) File.#{k}(f) end"
