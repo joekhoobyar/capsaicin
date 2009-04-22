1b22f0bb63b61a0c0f849220b52eb8da138a9b63
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-22 19:56:07 -0400

    more fixes

--- lib/capistrano_extensions/file_transfer.rb
+++ lib/capistrano_extensions/file_transfer.rb
@@ -1,21 +1,18 @@
-require 'capistrano/configuration/actions/file_transfer'
-
-module Capistrano
-  class Configuration
-    module Actions
-      module FileTransfer
-        def upload_tmp(from, options={}, &block)
-          to = "/tmp/%s.%4.4x-%d.tmp" % [ File.basename(to), rand(2**16), Time.now.utc.to_i ]
-          upload(from, to, options, &block)
-          return to unless block_given?
-          begin
-            yield
-          ensure
-            run "rm -f '#{to}'"
-          end
-        end
+module CapistranoExtensions
+  module FileTransfer
+    def upload_tmp(from, options={}, &block)
+      to = "/tmp/%s.%4.4x-%d.tmp" % [ File.basename(to), rand(2**16), Time.now.utc.to_i ]
+      upload(from, to, options, &block)
+      return to unless block_given?
+      begin
+        yield
+      ensure
+        run "rm -f '#{to}'"
       end
     end
   end
 end
 
+Capistrano::Configuration.class_eval do
+  include CapistranoExtensions::FileTransfer
+end
