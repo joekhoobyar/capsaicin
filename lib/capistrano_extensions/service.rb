0148d0f041e77c2b03308f50692d72a1ccd5ab41
Joe Khoobyar <joe@ankhcraft.com>
2009-04-22 00:26:28 -0400

    implemented simple LSB service plugin

--- lib/capistrano_extensions/service.rb
+++ lib/capistrano_extensions/service.rb
@@ -2,31 +2,42 @@ module Service
 
 	LSB_DEFAULT_ACTIONS = %w(start stop restart status)
 
-	SVC_ACTION_CAPTIONS = Hash.new(:status => 'Service Status', :check => 'Config Check') do |h,k|
+	SVC_ACTION_CAPTIONS = Hash.new do |h,k|
 		h[k] = "#{k.to_s.capitalize} Service"
 	end
+	SVC_ACTION_CAPTIONS.update :status => 'Check Status', :check => 'Check Config'
 
 	def ocf
 	end
 
 	def lsb(id,*args)
 		svc_desc = next_description(:reset)
-		svc_cmd = "/etc/init.d/#{id}"
+		svc_cmd = "/etc/init.d/#{id.to_s.split(':').last}"
 		svc_actions = LSB_DEFAULT_ACTIONS 
 
 		if Hash === args.last
 			options = args.pop
+			svc_desc = id.to_s.capitalize unless svc_desc or options.delete(:hide)
 			svc_actions += args.shift if Array === args.first
 		else
 			options = {}
 		end
 
-		svc_actions.each do |svc_action|
-			svc_action = svc_action.intern
-			desc "#{SVC_ACTION_CAPTIONS[svc_action]}: #{svc_desc}"
-			task svc_action, options do
-				sudo "#{svc_cmd} #{svc_action}"
+		namespace id do
+			desc "#{svc_desc}: #{SVC_ACTION_CAPTIONS[:status]}" if svc_desc
+			task :default, options do
+					sudo "#{svc_cmd} status"
 			end
+
+			svc_actions.each do |svc_action|
+				svc_action = svc_action.intern
+				desc "#{svc_desc}: #{SVC_ACTION_CAPTIONS[svc_action]}" if svc_desc
+				task svc_action, options do
+					sudo "#{svc_cmd} #{svc_action}"
+				end
+			end
+
+			instance_eval { yield } if block_given?
 		end
 	end
 
