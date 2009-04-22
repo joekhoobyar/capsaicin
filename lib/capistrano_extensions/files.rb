645bf03b0b0befaf6ae703752a553dc6d25ec455
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-22 19:45:58 -0400

    more fixes

--- lib/capistrano_extensions/files.rb
+++ lib/capistrano_extensions/files.rb
@@ -3,15 +3,16 @@ require 'fileutils'
 module CapistranoExtensions
   module Files
 
-    include FileUtils
+    include FileUtils::Verbose
 
-    public(*(FileUtils.methods(false) - %w(copy_entry copy_file copy_stream
-                                            remove_entry remove_entry_secure remove_file
-                                            compare_file compare_stream
-                                            uptodate?)))
+    public *FileUtils::Verbose.methods(false)
+    private *%w(copy_entry copy_file copy_stream
+                remove_entry remove_entry_secure remove_file
+                compare_file compare_stream
+                uptodate?)
 
     class_eval(%w(exists? directory? executable?).map do |m|
-      "def #{k}(f) File.#{k}(f) end"
+      "def #{m}(f) File.#{m}(f) end"
     end.join("\n"))
 
   end
