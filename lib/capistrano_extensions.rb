7c5bdeb74e5a844dd062dcd696cd157ed5dc3345
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-22 12:04:04 -0400

    more updates

---
+++ lib/capistrano_extensions.rb
@@ -0,0 +1,2 @@
+require "#{File.dirname(__FILE__)}/capistrano_extensions/file_transfer.rb"
+require "#{File.dirname(__FILE__)}/capistrano_extensions/service.rb"
--- lib/capistrano_extensions/file_transfer.rb
+++ lib/capistrano_extensions/file_transfer.rb
@@ -7,8 +7,14 @@ module Capistrano
         def upload_tmp(from, options={}, &block)
           to = "/tmp/%s.%4.4x-%d.tmp" % [ File.basename(to), rand(2**16), Time.now.utc.to_i ]
           upload(from, to, options, &block)
+          return to unless block_given?
+          begin
+            yield
+          ensure
+            run "rm -f '#{to}'"
+          end
         end
-	  end
+      end
     end
   end
 end
