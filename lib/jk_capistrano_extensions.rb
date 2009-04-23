978a2044d1504171d143558bf5dc6f5fdba4306b
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-23 00:42:27 -0400

    make it work in a gem too

--- lib/jk_capistrano_extensions.rb
+++ lib/jk_capistrano_extensions.rb
@@ -1,3 +1,8 @@
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "jk-capistrano-extensions requires Capistrano 2"
+end
+require 'capistrano'
+
 require "#{File.dirname(__FILE__)}/capistrano_extensions/file_transfer.rb"
 require "#{File.dirname(__FILE__)}/capistrano_extensions/files.rb"
 require "#{File.dirname(__FILE__)}/capistrano_extensions/service.rb"
