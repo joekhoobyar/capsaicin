af3c0330ac95f749747edaf3f7d90488bbba3000
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-23 00:31:10 -0400

    be able to load everything

--- lib/capistrano_extensions.rb
+++ lib/capistrano_extensions.rb
@@ -1,2 +1,3 @@
 require "#{File.dirname(__FILE__)}/capistrano_extensions/file_transfer.rb"
+require "#{File.dirname(__FILE__)}/capistrano_extensions/files.rb"
 require "#{File.dirname(__FILE__)}/capistrano_extensions/service.rb"
