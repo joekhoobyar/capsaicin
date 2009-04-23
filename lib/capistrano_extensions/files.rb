3fd22da3872c7e9e9a2fa3ce9f761bd3eaf0991d
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-22 23:05:04 -0400

    added support for files via :system, and added several fixes

--- lib/capistrano_extensions/files.rb
+++ lib/capistrano_extensions/files.rb
@@ -1,10 +1,16 @@
 require File.join(File.dirname(__FILE__), %w(files local.rb))
+require File.join(File.dirname(__FILE__), %w(files remote.rb))
+
+exists?(:files_via) or set(:files_via, :remote)
 
 module CapistranoExtensions::Files
   class_eval(Local.public_instance_methods(false).map do |m|
-    "def #{m}(*f) local_files.#{m}(*f) end"
+    "def #{m}(*f) send(_via.to_s + '_files').#{m}(*f) end"
   end.join("\n"))
+
+  def _via
+    (files_via.to_sym != :local) ? :remote : :local
+  end
 end
 
 Capistrano.plugin :files, CapistranoExtensions::Files
-
---
+++ lib/capistrano_extensions/files/remote.rb
@@ -0,0 +1,166 @@
+require 'fileutils'
+
+module CapistranoExtensions
+  module Files
+    module Remote
+
+      def cd(dir, options={})
+        if block_given?
+          dir, dir2 = pwd, dir
+          _r 'cd', dir2
+          yield
+        end  
+        _r 'cd', dir
+      end
+
+      def pwd
+        capture("pwd", :via => _via)
+      end
+
+      def mkdir(*args)
+        options = args.pop if Hash === args.last
+        _r 'mkdir', args
+      end
+
+      def mkdir_p(*args)
+        options = args.pop if Hash === args.last
+        _r 'mkdir -p', args
+      end
+
+      def rmdir(*args)
+        options = args.pop if Hash === args.last
+        _r 'rmdir', args
+      end
+
+      def ln(*args)
+        options = args.pop if Hash === args.last
+        _r 'ln', args, 2
+      end
+
+      def ln_s(*args)
+        options = args.pop if Hash === args.last
+        _r 'ln -s', args, 2
+      end
+
+      def ln_sf(*args)
+        options = args.pop if Hash === args.last
+        _r 'ln -sf', args, 2
+      end
+
+      def cp(*args)
+        options = args.pop if Hash === args.last
+        _r 'cp', args
+      end
+
+      def cp_r(*args)
+        options = args.pop if Hash === args.last
+        _r 'cp -r', args
+      end
+
+      def mv(*args)
+        options = args.pop if Hash === args.last
+        _r 'mv', args, 2
+      end
+
+      def rm(*args)
+        options = args.pop if Hash === args.last
+        _r 'rm', args
+      end
+
+      def rm_r(*args)
+        options = args.pop if Hash === args.last
+        _r 'rm -r', args
+      end
+
+      def rm_rf(*args)
+        options = args.pop if Hash === args.last
+        _r 'rm -rf', args
+      end
+
+      def install(*args)
+        options = args.pop if Hash === args.last
+        _r 'install', args, 2
+      end
+
+      def chmod(*args)
+        options = args.pop if Hash === args.last
+        _r 'chmod', args
+      end
+
+      def chmod_R(*args)
+        options = args.pop if Hash === args.last
+        _r 'chmod -R', args
+      end
+
+      def chown(*args)
+        options = args.pop if Hash === args.last
+        _r 'chown', args
+      end
+
+      def chown_R(*args)
+        options = args.pop if Hash === args.last
+        _r 'chown -R', args
+      end
+
+      def touch(*args)
+        options = args.pop if Hash === args.last
+        _r 'touch', args
+      end
+
+      def exists?(a)
+        _r "[ -f #{_q a} ]"
+      end
+
+      def directory?(a)
+        _r "[ -d #{_q a} ]"
+      end
+
+      def executable?(a)
+        _r "[ -x #{_q a} ]"
+      end
+
+
+    private
+
+      def _r(cmd, args=nil, min=nil)
+        case args
+        when NilClass
+          raise ArgumentError unless min.nil? or min.zero?
+        when Array
+          args = args.flatten
+          raise ArgumentError if (min || 1) > args.length
+          cmd = "#{cmd} #{_q(*args)}" if args.any?
+        else
+          raise ArgumentError if min.nil? or min < 1
+          cmd = "#{cmd} #{_q args}"
+        end
+
+        case (v = _via)
+        when :system
+          $stderr.puts cmd
+          system cmd
+        else
+          invoke_command cmd, :via => _via
+        end
+      end
+
+      def _q(*list)
+        list.map { |l| "'#{l.gsub("'", "\\'")}'" }.join ' '
+      end
+
+      def _via
+        case (v = files_via)
+        when :local
+          :system
+        when :remote, NilClass
+          (get(:run_method) if exists?(:run_method)) || :run
+        else 
+          v
+        end
+      end
+
+    end
+  end
+end
+
+Capistrano.plugin :remote_files, CapistranoExtensions::Files::Remote
