af57fba1dc356fdbecc58694c07c3105c1cbcd95
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-04-27 11:22:11 -0400

    not the best place for this, but just for now...

---
+++ lib/capistrano_extensions/sys.rb
@@ -0,0 +1,11 @@
+module CapistranoExtensions
+  module LocalSys
+
+    def windows?
+      @windows ||= !! (RUBY_PLATFORM =~ /cygwin|mingw|win32|mswin|windows/)
+    end
+
+  end
+
+  Capistrano.plugin :local_sys, LocalSys
+end
