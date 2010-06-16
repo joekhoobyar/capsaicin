128fc652b4515171ad529110be88debbfeae7d51
Joe Khoobyar <joe@ankhcraft.com>
2010-06-15 22:23:48 -0400

    basic invocation tests

--- test/helper.rb
+++ test/helper.rb
@@ -2,8 +2,13 @@ require 'stringio'
 require 'test/unit'
 require 'rubygems'
 require 'capistrano/logger'
+require 'capistrano/configuration/variables'
 
 class CapistranoMock
+
+  include Capistrano::Configuration::Variables
+  
+  attr_reader :invocations
 
   def logbuf
     @logbuf ||= StringIO.new
@@ -15,6 +20,16 @@ class CapistranoMock
       @logger.level = Capistrano::Logger::MAX_LEVEL
     end
     @logger
+  end
+
+  %w(invoke_command capture stream).each do |k|
+    class_eval %Q{
+  def #{k}(*args, &block)
+    args = [args]
+    args << block if block
+    ((@invocations ||= {})[:#{k}] ||= []) << args
+  end
+}
   end
 
 end
