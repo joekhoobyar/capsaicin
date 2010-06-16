35de44114bb4233521038cfd78e1a3e75f77def1
Joe Khoobyar <joe@ankhcraft.com>
2010-06-15 22:04:08 -0400

    allow overriding options

--- lib/capsaicin/invocation.rb
+++ lib/capsaicin/invocation.rb
@@ -5,24 +5,24 @@ module Capsaicin
     # Automatically uses the :run_method variable to run things.
     # Equivalent to +invoke_command *args, :via=>fetch(:run_method, :run)+
     def vrun(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
-      options[:via] = fetch(:run_method, :run)
+      options = Hash===args.last ? args.pop.dup : {}
+      options[:via] ||= fetch(:run_method, :run)
       invoke_command *args.push(options), &block
     end
 
     # Automatically uses the :run_method variable to run things.
     # Equivalent to +capture *args, :via=>fetch(:run_method, :run)+
     def vcapture(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
-      options[:via] = fetch(:run_method, :run)
+      options = Hash===args.last ? args.pop.dup : {}
+      options[:via] ||= fetch(:run_method, :run)
       capture *args.push(options), &block
     end
 
     # Automatically uses the :run_method variable to run things.
     # Equivalent to +stream *args, :via=>fetch(:run_method, :run)+
     def vstream(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
-      options[:via] = fetch(:run_method, :run)
+      options = Hash===args.last ? args.pop.dup : {}
+      options[:via] ||= fetch(:run_method, :run)
       stream *args.push(options), &block
     end
 
@@ -46,24 +46,26 @@ module Capsaicin
     # Always uses :runner to sudo as someone. 
     # Equivalent to:  sudo "command", :as => fetch(:runner,nil)
     def sudo_as(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
-      options[:as] = fetch(:runner, nil)
+      options = Hash===args.last ? args.pop.dup : {}
+      options[:as] ||= fetch(:runner, nil)
       sudo *args.push(options), &block
     end
     
     # Extremely helpful if you only have permission to: sudo su SOMEUSER -c "command"
     def sudo_su(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
-      args[0] = "su #{fetch(:runner, nil)} -c '#{args[0]}'"
+      options = Hash===args.last ? args.pop.dup : {}
+      as      = options.delete(:as) || fetch(:runner, nil)
+      args[0] = "su #{as} -c '#{args[0]}'"
       sudo *args.push(options), &block
     end
     
     # Extremely helpful if you only have permission to: sudo su - SOMEUSER
     def sudo_su_to(*args, &block)
-      options = Hash===args.last ? args.pop.dup :  {}
+      options = Hash===args.last ? args.pop.dup : {}
       options[:shell] = false
-      cmd = args[0].gsub(/[$\\`"]/) { |m| "\\#{m}" }
-      args[0] = "echo \"#{cmd}\" | #{sudo} su - #{fetch(:runner, nil)}"
+      cmd     = args[0].gsub(/[$\\`"]/) { |m| "\\#{m}" }
+      as      = options.delete(:as) || fetch(:runner, nil)
+      args[0] = "echo \"#{cmd}\" | #{sudo} su - #{as}"
       run *args.push(options), &block
     end
   end
