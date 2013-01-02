a0b956afbcd0c81bcdfa2920ba5f777f08d5e504
Joe Khoobyar <jkhoobyar@tnsi.com>
2013-01-02 03:54:43 -0500

    working on specialized copy strategy..

---
+++ lib/capistrano/recipes/deploy/strategy/copy_package.rb
@@ -0,0 +1,61 @@
+require 'capistrano/recipes/deploy/strategy/copy'
+
+module Capistrano # :nodoc:
+  module Deploy # :nodoc:
+    module Strategy # :nodoc:
+      
+      # Specialized copy strategy that expects the compressed package to exist after the source is built.
+      class CopyPackage < Copy
+
+		    # Returns the location of the local package file.
+		    # Returns +nil+ unless :package_file has been set. If :package_file
+		    # is +true+, a default file location will be returned.
+		    def package_file
+		      @package_file ||= begin
+		        file = configuration[:package_file]
+		        file = "#{package_name}.#{compression.extension}" if TrueClass === file
+		        File.expand_path(configuration[:package_file], copy_dir) rescue nil
+		      end
+		    end
+
+		    # Returns the package name.  Used as a default basename for :package_file
+        def package_name
+          @package_name ||= configuration[:package_name] || "#{configuration[:application]}-#{File.basename(destination)}"
+        end
+        
+      private
+
+	      # Returns the name of the package file to be considered.
+	      def filename
+	        @filename ||= package_file || File.join(copy_dir, "#{package_name}.#{compression.extension}")
+	      end
+
+        # Returns the value of the :copy_dir variable, defaulting to the current directory.
+        def copy_dir
+          @copy_dir ||= configuration[:copy_dir] || '.'
+        end
+
+        # Don't build an archive file.
+        def compress_repository        ; end
+        def copy_cache_to_staging_area ; end
+        def remove_excluded_files      ; end
+        def rollback_changes           ; end
+                    
+#     
+#        def xx
+#          logger.debug "compressing local copy to #{filename}"
+#          local_files.tar_cz filename, '.', :verbose=>false do |item|
+#            if (item=item[2..-1]) and item.length > 0
+#              name = File.basename item
+#              name == "." || name == ".." || copy_prune.include?(name) ||
+#                copy_exclude.any?{|p| File.fnmatch(p, item, File::FNM_DOTMATCH)} ||
+#                (! File.directory?(item) && copy_directory_only.any?{|p| item[0,p.length+1]==p+'/'})
+#            end
+#          end
+#	      end
+      
+      end
+
+    end
+  end
+end
